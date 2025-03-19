6301bde2ecaebabcce5e5ad964aaef9085dbef96
fernandes-draw <fernandes_draw@hotmail.com>
2025-03-18 22:45:32 -0300

    Server Actions & Bind SQL Data - Class 8

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,20 +1,19 @@
 import ProductList from '@/components/shared/products/product-list';
-import { Button } from '@/components/ui/button';
-import sampleData from '@/db/sample-data';
+import { getLatestProducts } from '@/db/actions/product.actions';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: `Home`,
 };
 
-const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-
 const HomePage = async () => {
-  await delay(2000);
+  const latestProducts = await getLatestProducts();
 
-  return <>
-    <ProductList data={sampleData.products} title='Latest Arrivals' limit={4} />
-  </>;
+  return (
+    <>
+      <ProductList data={latestProducts} title='Latest Arrivals' />
+    </>
+  );
 };
 
 export default HomePage;
