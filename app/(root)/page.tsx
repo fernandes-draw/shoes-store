b00c87bb4177d73b3803a725fd4556d4cf969e3c
fernandes-draw <fernandes_draw@hotmail.com>
2025-04-14 12:44:31 -0300

    Session_05 - Binding Sample Products

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,6 @@
 import { Metadata } from 'next'
+import sampleData from '@/db/sample-data'
+import ProductsList from '@/components/shared/products/product-list'
 
 export const metadata: Metadata = {
   title: `Home`,
@@ -9,7 +11,17 @@ const delay = (ms: number) => new Promise((res) => setTimeout(res, ms))
 const HomePage = async () => {
   await delay(1000)
 
-  return <>HomePage</>
+  console.log(sampleData)
+
+  return (
+    <>
+      <ProductsList
+        data={sampleData.products}
+        title='Latest Arrivals'
+        limit={4}
+      />
+    </>
+  )
 }
 
 export default HomePage
